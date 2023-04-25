95e45084007d156c4ead023ad9a727187087765d
facuvies <facundovies@gmail.com>
2023-04-25 19:27:01 -0300

    reactcalculator

---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+import "../hojas-de-estilo/boton-clear.css";
+
+const BotonClear = (props) => (
+
+    <div className="boton-clear"
+    onClick={props.manejarClear}>
+        {props.children}
+    </div>
+)
+
+export default BotonClear;
